658aa6363f593825d53874735f2e116eb31fa311
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-11-25 13:34:08 -0300

    Modelo SensorAcquisition

--- sensors/models.py
+++ sensors/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class SensorAcquisition(models.Model):
+    id = models.AutoField(primary_key=True)
+    platform_id = models.CharField(max_length=100)
+    fetch_datetime = models.DateTimeField()
+    datetime_received = models.DateTimeField()
+    sensor_acquisitions = models.FloatField()
+
+    def __str__(self):
+        return f"{self.platform_id} - {self.fetch_datetime}"
